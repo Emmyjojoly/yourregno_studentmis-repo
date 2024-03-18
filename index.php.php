24c585f3572e79db8b83557af1f5de7a7b8a5d67
Emmy jojoly <emmyjojoly@gmail.com>
2024-03-18 06:35:53 -0700

    index page is created

---
+++ index.php.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>school MS</title>
+</head>
+<body>
+
+<h1>Welcome to school</h1>
+<p>This is a paragraph.</p>
+<a href="https://www.yourhomepage.com"> </a>
+</body>
+</html>
